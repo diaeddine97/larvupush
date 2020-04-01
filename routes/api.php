f93524fa7f9567365810b79840fda56b5d5743f7
diae <diaeddine97@gmail.com>
2020-04-01 12:43:52 +0100

    CRUD for Reply + Resource for reply and category)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ReplyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
+Route::apiResource('/question/{question}/reply', 'ReplyController');
